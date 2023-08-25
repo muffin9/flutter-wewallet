f350f9c575cd1e3b6412aca2e78394ceb6f01a2d
sungjin.kim@voithru.com <sungjin.kim@voithru.com>
2023-08-26 00:08:06 +0900

    folder setting

---
+++ android/app/src/main/kotlin/com/example/flutter_wewallet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_wewallet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_wewallet/home/Home.dart';
+
+void main() {
+  runApp(const _App());
+}
+
+class _App extends StatelessWidget {
+  const _App();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(fontFamily: 'NotoSans'),
+        debugShowCheckedModeBanner: false,
+        // home: const SplashScreen());
+        home: const Home());
+  }
+}
